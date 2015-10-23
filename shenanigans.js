3c1d2c80a0980612497f9812845fd1fa3b5e0cdf
calebmer <calebmeredith8@gmail.com>
2015-10-23 15:58:07 -0400

    Add options

--- shenanigans.js
+++ shenanigans.js
@@ -7,19 +7,30 @@ const $HINT = /\((.*)\)/
 const $IS_NUM = /\d+/
 
 const Shenanigans = {
-  parse(message) {
+  parse(message, options) {
+    options = options || {}
+    const statusCode = options.statusCode || true
+    const hint = options.hint || true
+    const assertion = options.assertion || true
+
+    if (message instanceof Error) {
+      if (assertion && message.name === 'AssertionError') {
+        message = (statusCode ? '404: ' : '') + (hint ? `Failed assertion (${message.message})` : message.message)
+      }
+    }
+
     if (typeof message === 'object' && 'message' in message) {
       message = message.message
     }
 
     const info = {}
 
-    if (message.match($STATUS_CODE)) {
+    if (statusCode && message.match($STATUS_CODE)) {
       info.statusCode = parseInt($STATUS_CODE.exec(message)[1], 10)
       message = message.replace($STATUS_CODE, '') || STATUS_CODES[info.statusCode]
     }
 
-    if (message.match($HINT)) {
+    if (hint && message.match($HINT)) {
       info.hint = $HINT.exec(message)[1]
       message = message.replace($HINT, '')
     }
